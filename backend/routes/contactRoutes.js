d698ce31e2e7443bf145888d5bd7a49b7b49ddc8
rohanshinde1702 <rohan.shinde@futuretechpartner.com>
2026-09-11 11:23:08 +0530

    Structure workspace into exact 2-folder setup: real-website (MERN stack with MongoDB & real images) and themeforest-template (standalone React template with placeholders & docs)

--- backend/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getContacts, submitContact } = require("../controllers/contactController");
-
-router.get("/", getContacts);
-router.post("/", submitContact);
-
-module.exports = router;
--- backend/routes/settingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getSettings, updateSettings } = require("../controllers/settingController");
-
-router.get("/", getSettings);
-router.put("/", updateSettings);
-
-module.exports = router;
